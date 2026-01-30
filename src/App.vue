54f41c92b345b7e13e011dd4bb14084c0a3fe38f
Johan Abbors <jabbors@gmail.com>
2026-01-30 22:40:54 +0200

    Example menu with router and views

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-  <span>Lotto</span>
+  <header class="header">
+    <button class="hamburger" @click="toggleMenu">☰</button>
+
+    <nav v-if="menuOpen" class="menu">
+      <router-link to="/" @click="closeMenu">Home</router-link>
+      <router-link to="/about" @click="closeMenu">About</router-link>
+      <router-link to="/contact" @click="closeMenu">Contact</router-link>
+    </nav>
+  </header>
+
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   name: 'App',
   components: {
+  },
+  setup() {
+    const menuOpen = ref(false)
+
+    const toggleMenu = () => {
+      menuOpen.value = !menuOpen.value
+    }
+
+    const closeMenu = () => {
+      menuOpen.value = false
+    }
+
+    return { menuOpen, toggleMenu, closeMenu }
   }
 }
 </script>
@@ -19,5 +45,34 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header {
+  position: relative;
+  padding: 1rem;
+  background: #f5f5f5;
+}
+
+.hamburger {
+  font-size: 24px;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.menu {
+  position: absolute;
+  top: 3rem;
+  left: 1rem;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 1rem;
+}
+
+.menu a {
+  margin-bottom: 0.5rem;
+  text-decoration: none;
 }
 </style>
