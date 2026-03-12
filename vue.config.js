c76545c480ff80f6920d9fed6721788c218efa71
Johan Abbors <jabbors@gmail.com>
2026-03-12 23:22:53 +0200

    Configure project, prefer local development without any changes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  publicPath: '',
+  publicPath: '/',
   chainWebpack: config => {
     config
       .plugin('html')
